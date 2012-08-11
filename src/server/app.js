4c5bc386dd985f6fec0fbe742eccba0b03ccb6a5
Andrew Benz <andorbal@gmail.com>
2012-08-11 18:18:41 -0500

    Built a very simple socket.io example

---
+++ src/server/app.js
@@ -0,0 +1,26 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/../client/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.on('click event', function(data) {
+    if (data.function == 'time') {
+      socket.emit('time event', {time: new Date()});
+    }
+  });
+});
